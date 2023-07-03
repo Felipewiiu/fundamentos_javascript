7e25ce071405593cc1e1b63a2a12cd4483b8e0a7
Felipe Lima <felipe.lima@FLEX.LOCAL>
2023-07-03 14:19:39 -0300

    finalizando o projeto

--- AULA5/filtrar.js
+++ AULA5/filtrar.js
@@ -1,3 +1,17 @@
 const clientes = require('./clientes.json');
 
-console.log(clientes)
+// hasOwnProperty --> é a função que verifica se um atributo tem um valor
+
+function filtrarApartamentoSemComplemento(clientes){
+    return clientes.filter(cliente =>{
+        return cliente.endereco.apartamento && !cliente.endereco.hasOwnProperty("complemento")
+    })
+}
+
+const filtrados = filtrarApartamentoSemComplemento(clientes)
+
+console.log(filtrados)
+
+// const alvo = clientes[0].endereco.complemento
+
+// console.log(alvo)
